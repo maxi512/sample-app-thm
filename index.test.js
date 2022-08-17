5d85b47979052a6b46a501cc768fdc3f70497204
Tomas Scandalitta <38343557+tscandalitta@users.noreply.github.com>
2022-08-17 17:50:16 -0300

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -5,7 +5,7 @@ const app = require("./index");
 describe("Test example", () => {
     test("GET /", async () => {
         const res = await request(app).get('/');
-        expect(res.body.name).toBe('Hello World!');
+        expect(res.body.name).toBe('Hello World!!!!!');
     });
 });
 
