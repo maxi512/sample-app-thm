677b44eb9a5b1de1b582c23fa2d501fec780820f
Tomas Scandalitta <38343557+tscandalitta@users.noreply.github.com>
2022-08-18 19:41:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send({name: 'Test y deploy exitosos'})
+  res.send({name: 'Fallaron los tests'})
 })
 
 let server = app.listen(port, () => {
